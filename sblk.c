67cc810d2089b428eb3c839725037b897c63e7e6
Lars Brinkhoff <lars@nocrew.org>
2017-01-11 12:13:36 +0100

    Remove unused function.

--- sblk.c
+++ sblk.c
@@ -20,48 +20,6 @@
 #include "memory.h"
 
 void
-dis_sblk (FILE *f, int cpu_model)
-{
-  int address;
-  word_t word;
-  int i;
-
-  int block_length, block_address;
-
-  printf ("SLBK format\n\n");
-
-  address = 0;
-  while ((word = get_word (f)) != JRST_1)
-    {
-      address++;
-      if (address > 100)
-	{
-	  fprintf (stderr, "JRST 1 instruction not found in the first "
-		   "100 words\n");
-	  exit (1);
-	}
-    }
-      
-  while ((word = get_word (f)) & SIGNBIT)
-    {
-      block_length = -((word >> 18) | ((-1) & ~0777777));
-      block_address = word & 0777777;
-
-      for (i = 0; i < block_length; i++)
-	{
-	  disassemble_word (NULL, get_word (f),
-			    block_address + i, cpu_model);
-	}
-
-      word = get_word (f);
-      printf ("checksum: %012llo\n", word);
-    }
-
-  printf ("\n");
-  sblk_info (f, word, cpu_model);
-}
-
-void
 read_sblk (FILE *f, struct pdp10_memory *memory, int cpu_model)
 {
   int address;
