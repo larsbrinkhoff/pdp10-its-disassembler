a0a8d3b35f03d9e5bea9bd34e92e041b6a3fc009
Lars Brinkhoff <lars@nocrew.org>
2018-04-15 22:38:37 +0200

    Handle old archive files.

--- itsarc.c
+++ itsarc.c
@@ -19,11 +19,14 @@
 
 #include "dis.h"
 
+#define VERY_OLD_ARC ((word_t)(0777777777777LL))
 #define OLD_ARC ((word_t)(0416243010101LL)) /* Sixbit ARC!!! */
 #define NEW_ARC ((word_t)(0416243210101LL)) /* Sixbit ARC1!! */
 
 #define LEFT 0777777000000LL
 #define RIGHT 0777777LL
+
+static int old = 0;
 
 extern word_t get_its_word (FILE *f);
 
@@ -38,23 +41,6 @@ static void usage (const char *x)
 
 static void (*write_word) (FILE *, word_t);
 static void (*flush_word) (FILE *);
-
-static void
-write_aa_word (FILE *f, word_t word)
-{
-  fputc ((word >> 29) & 0177, f);
-  fputc ((word >> 22) & 0177, f);
-  fputc ((word >> 15) & 0177, f);
-  fputc ((word >>  8) & 0177, f);
-  fputc (((word >> 1) & 0177) +
-	 ((word << 7) & 0200), f);
-}
-
-static void
-flush_aa_word (FILE *f)
-{
-  (void)f;
-}
 
 static void
 massage (char *filename)
@@ -122,6 +108,93 @@ extract_file (char *filename, word_t *data, word_t length)
   fclose (f);
 }
 
+static int
+ildb (word_t **w, int *p)
+{
+  word_t b = **w;
+  b = b >> (30 - 6*(*p));
+  b &= 077;
+
+  (*p)++;
+  if (*p == 6)
+    {
+      *p = 0;
+      (*w)++;
+    }
+
+  return (int)b;
+}
+
+static int
+extract_block (FILE *f, word_t *block, int *b, int *count)
+{
+  word_t header = *block;
+  int i, n;
+
+  *b = header & 017777777;
+  n = ((header >> 23) & 01777) + 1;
+  *count += n;
+
+  if (f)
+    {
+      block++;
+      for (i = 0; i < n; i++)
+	write_word (f, block[i]);
+    }
+
+  return (header & 0200000000000LL) == 0;
+}
+
+static int
+extract_blocks (FILE *f, word_t *ufd, int undscp)
+{
+  word_t *d;
+  int o, b, n, n2, n3;
+  int count = 0;
+
+  d = &ufd[11+undscp/6];
+  o = undscp % 6;
+
+  n = ildb (&d, &o);
+  if (n != 040)
+    {
+      fprintf (stderr, "ERROR\n");
+      exit (1);
+    }
+  
+  n2 = ildb (&d, &o);
+  n3 = ildb (&d, &o);
+  b = ((n & 037) << 12) + (n2 << 6) + n3;
+
+  b = buffer[02005+b];
+  while (extract_block (f, &buffer[b], &b, &count))
+    ;
+
+  return count;
+}
+
+static int
+extract_old_file (char *filename, int i, int extract)
+{
+  word_t *ufd = buffer;
+  int undscp = ufd[i+2] & 017777;
+  FILE *f = NULL;
+  int n;
+
+  if (extract)
+    f = fopen(filename, "wb");
+
+  n = extract_blocks (f, ufd, undscp);
+
+  if (extract)
+    {
+      flush_word (f);
+      fclose (f);
+    }
+
+  return n;
+}
+
 int
 main (int argc, char **argv)
 {
@@ -166,12 +239,21 @@ main (int argc, char **argv)
 
   /* word_t arc_size = p - buffer; */
 
-  if (buffer[0] == OLD_ARC)
+  if (buffer[0] == NEW_ARC)
     {
-      fprintf (stderr, "Old ARC format.  Not supported.\n");
-      exit (1);
+      /* fprintf (stderr, "New ARC1!! archive.\n") */ ;
     }
-  else if (buffer[0] != NEW_ARC)
+  else if (buffer[0] == OLD_ARC)
+    {
+      fprintf (stderr, "Old ARC!!! archive.\n");
+      old = 1;
+    }
+  else if (buffer[0] == VERY_OLD_ARC)
+    {
+      fprintf (stderr, "Old 777777777777 archive.\n");
+      old = 1;
+    }
+  else
     {
       sixbit_to_ascii(buffer[0], string);
       fprintf (stderr, "First word: %012llo \"%s\"\n", buffer[0], string);
@@ -179,19 +261,26 @@ main (int argc, char **argv)
       exit (1);
     }
 
-  word_t name_beg = buffer[1];
+  word_t name_beg;
+  if (old)
+     name_beg = buffer[2];
+  else
+     name_beg = buffer[1];
   /* word_t data_end = buffer[2]; */
 
   fprintf (stderr, "Last cleanup: ");
   print_datime (stderr, buffer[3]);
   fputc ('\n', stderr);
 
-  fprintf (stderr, "Created: ");
-  print_datime (stderr, buffer[4]);
-  fputc ('\n', stderr);
+  if (!old)
+    {
+      fprintf (stderr, "Created: ");
+      print_datime (stderr, buffer[4]);
+      fputc ('\n', stderr);
 
-  word_t dumped = buffer[5];
-  fprintf (stderr, "Dumped: %llo\n", dumped);
+      word_t dumped = buffer[5];
+      fprintf (stderr, "Dumped: %llo\n", dumped);
+    }
 
   fprintf (stderr, "\nFile name       Words  Modified            Referenced    Byte\n");
 
@@ -209,7 +298,13 @@ main (int argc, char **argv)
       /* word_t flags = buffer[i+2] >> 18; */
       word_t data = buffer[i+2] & RIGHT;
 
-      word_t length = buffer[data] - 3;
+      massage (filename);
+
+      word_t length;
+      if (old)
+	length = extract_old_file (filename, i, extract);
+      else
+	length = buffer[data] - 3;
       fprintf (stderr, "%6lld  ", length);
 
       modified = buffer[i+3];
@@ -219,17 +314,19 @@ main (int argc, char **argv)
       print_date (stderr, referenced);
       fputc (')', stderr);
 
-      int author = (buffer[i+4] >> 9) & 0777;
-      if (author != 0 && author != 0777)
-	fprintf (stderr, "  Author %03o", author);
+      if (!old)
+	{
+	  int author = (buffer[i+4] >> 9) & 0777;
+	  if (author != 0 && author != 0777)
+	    fprintf (stderr, "  Author %03o", author);
+	}
 
       int leftovers;
       fprintf (stderr, "  %d\n",
 	       byte_size (buffer[i+4] & 0777, &leftovers));
 
-      if (extract)
+      if (!old && extract)
 	{
-	  massage (filename);
 	  extract_file (filename, &buffer[data+3], length);
 	  timestamps (filename, modified, referenced);
 	}
