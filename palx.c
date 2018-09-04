2055ae47c8c9955c3b85f066a7065a41d3652492
Lars Brinkhoff <lars@nocrew.org>
2018-09-04 21:14:39 +0200

    PALX binary converter can also write plain binary image.

--- palx.c
+++ palx.c
@@ -19,10 +19,14 @@
 
 #include "dis.h"
 
+int absolute = 0;
+int image = 0;
+void (*out_fn) (FILE *, FILE *, int, int, int);
+
 static void
 usage (const char *x)
 {
-  fprintf (stderr, "Usage: %s -W<format>\n", x);
+  fprintf (stderr, "Usage: %s -I|A [-W<format>]\n", x);
   exit (1);
 }
 
@@ -46,16 +50,10 @@ pdp11_out (FILE *f, int word)
   fputc ((word >> 8) & 0377, f);
 }
 
-static void
-block (FILE *in, FILE *out)
+static void absolute_out (FILE *in, FILE *out, int first,
+			  int address, int count)
 {
-  int c, i, first, count, address;
-
-  for (first = 0; ((first = pdp11_in (in)) & 0377) != 1; )
-    ;
-  
-  count = pdp11_in (in);
-  address = pdp11_in (in);
+  int i, c;
 
   pdp11_out (out, first);
   pdp11_out (out, count);
@@ -74,6 +72,46 @@ block (FILE *in, FILE *out)
   fputc (c & 0377, out);
 }
 
+static void image_out (FILE *in, FILE *out, int first, int address, int count)
+{
+  static int last = -1;
+  int i, c;
+  
+  if (count == 6)
+    exit (0);
+
+  if (last != -1)
+    {
+      for (i = last; i < address; i++)
+	pdp11_out (out, 0);
+    }
+
+  last = address;
+
+  for (i = 6; i < count; i++)
+    {
+      c = get_word (in);
+      fputc (c & 0377, out);
+      last++;
+    }
+
+  c = get_word (in);
+}
+
+static void
+block (FILE *in, FILE *out)
+{
+  int first, count, address;
+
+  for (first = 0; ((first = pdp11_in (in)) & 0377) != 1; )
+    ;
+  
+  count = pdp11_in (in);
+  address = pdp11_in (in);
+
+  out_fn (in, out, first, address, count);
+}
+
 int
 main (int argc, char **argv)
 {
@@ -81,13 +119,19 @@ main (int argc, char **argv)
 
   file_36bit_format = FORMAT_ITS;
 
-  while ((opt = getopt (argc, argv, "W:")) != -1)
+  while ((opt = getopt (argc, argv, "AIW:")) != -1)
     {
       switch (opt)
 	{
 	case 'W':
 	  if (parse_word_format (optarg))
 	    usage (argv[0]);
+	  break;
+	case 'A':
+	  absolute = 1;
+	  break;
+	case 'I':
+	  image = 1;
 	  break;
 	default:
 	  usage (argv[0]);
@@ -96,6 +140,17 @@ main (int argc, char **argv)
 
   if (optind != argc)
     usage ("optind");
+
+  if ((absolute ^ image) == 0)
+    {
+      fprintf (stderr, "Must specify one of -A or -I for output format.\n");
+      exit (1);
+    }
+
+  if (absolute)
+    out_fn = absolute_out;
+  if (image)
+    out_fn = image_out;
 
   for (;;)
     block (stdin, stdout);
