5d622cbde9b6bc729f3621f380ad62cd77040416
Lars Brinkhoff <lars@nocrew.org>
2019-07-10 10:31:21 +0200

    Update MAGDMP to use common tape record code.

--- magdmp.c
+++ magdmp.c
@@ -17,63 +17,15 @@
 
 #include "dis.h"
 
-extern word_t get_core_word (FILE *f);
-
-static int
-get_byte (FILE *f)
-{
-  int c = fgetc (f);
-  return c == EOF ? 0 : c;
-}
-
-static int get_reclen (FILE *f)
-{
-  return get_byte (f) |
-    ((word_t)get_byte (f) << 8) |
-    (get_byte (f) << 16) |
-    (get_byte (f) << 24);
-}
-
-static int get_record (FILE *f, void *buffer)
-{
-  int i, x, reclen;
-  word_t *p = buffer;
-
-  reclen = get_reclen (f);
-  if (reclen == 0)
-    return 0;
-  if (reclen % 5)
-    {
-      fprintf (stderr, "Not a CORE DUMP tape image.\n"
-	       "reclen = %d\n", reclen);
-      exit (1);
-    }
-  
-  for (i = 0; i < (reclen / 5); i++)
-    *p++ = get_core_word (f);
-
-  if (reclen & 1)
-    get_byte (f);
-
-  x = get_reclen (f);
-  if (x != reclen)
-    {
-      fprintf (stderr, "Error in tape image format.\n"
-	       "%d != %d\n", reclen, x);
-      exit (1);
-    }
-
-  return reclen / 5;
-}
-
-word_t buffer[5 * 1024];
-
 int
 main (int argc, char **argv)
 {
+  word_t *buffer;
   word_t word;
   FILE *f;
   int eof = 0;
+  int files = 0;
+  int words = 0;
 
   if (argc != 2)
     {
@@ -86,10 +38,16 @@ main (int argc, char **argv)
   for (;;)
     {
       int n;
-      n = get_record (f, buffer);
-      fprintf (stderr, "record: %d words\n", n);
+      n = get_9track_record (f, &buffer);
+      words += n;
       if (n == 0)
 	{
+          files++;
+          if (files & 1)
+            {
+              fprintf (stderr, " %d words\n", words);
+            }
+          words = 0;
 	  if (eof)
 	    exit (0);
 	  eof = 1;
@@ -98,12 +56,21 @@ main (int argc, char **argv)
 	{
 	  char ascii[8];
 	  eof = 0;
-	  sixbit_to_ascii (buffer[0], ascii);
-	  fprintf (stderr, "%s ", ascii);
-	  sixbit_to_ascii (buffer[1], ascii);
-	  fprintf (stderr, "%s ", ascii);
-	  sixbit_to_ascii (buffer[2], ascii);
-	  fprintf (stderr, "%s\n", ascii);
+          if (files == 0)
+            {
+              fprintf (stderr, "Boot record:");
+            }
+          if (files & 1)
+            {
+              fprintf (stderr, "File %d: ", (files >> 1) + 1);
+              sixbit_to_ascii (buffer[0], ascii);
+              fprintf (stderr, "%s ", ascii);
+              sixbit_to_ascii (buffer[1], ascii);
+              fprintf (stderr, "%s ", ascii);
+              sixbit_to_ascii (buffer[2], ascii);
+              fprintf (stderr, "%s", ascii);
+            }
+          free (buffer);
 	}
     }
 
