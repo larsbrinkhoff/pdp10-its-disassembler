389d216355eb388e6f84a46552d596bb4727d78d
Adam Sampson <ats@offog.org>
2019-11-23 19:11:33 +0000

    strcasestr needs _GNU_SOURCE on newer glibc.
    It's a non-standard (but fairly common) function.

--- dis.c
+++ dis.c
@@ -13,6 +13,8 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
 
+#define _GNU_SOURCE /* for strcasestr */
+
 #include <stdio.h>
 #include <string.h>
 
